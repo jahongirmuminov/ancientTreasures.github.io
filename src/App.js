44a420fd8e8f54e35c14dcdf9e1ff50ed2b98f6b
jahongirmuminov <muminov707@gmail.com>
2021-05-31 14:11:25 +0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Sale from './components/Sale';
+import NavbarJS from './Navbar/NavbarJS';
 function App() {
   return (
-    <div className="container">
-        <h1>Hello wordl</h1>
-       
-    </div> 
+    <Router>
+       <NavbarJS/>
+         <Switch>
+          <Route exact  path="/sale" component={Sale} />
+        
+             
+           
+        </Switch>
+      </Router>
+  
   );
 }
 
---
+++ src/Navbar/NavbarJS.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Nav,Form,Button,FormControl,NavDropdown, Container, Row, Col, NavLink} from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link } from 'react-router-dom'
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+const NavbarJS = () => {
+
+    
+    return (
+        <>
+       <Navbar  expand="lg" variant="light" >
+         <div className="container">             
+    <Navbar.Brand href="#"><img src="/images/image.png"  style={{marginTop:"25px"}}  alt="none"/></Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="navbarRight"
+      // navbarScroll
+    >
+       <ul className="navbarItem" type="none">
+          <li className="listItem">
+            <Link className="navLinkItem" to="/vikings">Vikings</Link>
+          </li>
+          <li className="listItem">
+            <Link className="navLinkItem" to="/egypt">Ancient Egypt</Link>
+          </li>
+          <li className="listItem">
+            <Link className="navLinkItem " to="/celtics">Celtics</Link>
+          </li>
+          <li className="listItem">
+            <Link className="navLinkItem active" style={{textTransform:"uppercase"}} to="/sale">Sale</Link>
+          </li>
+          <li className="listItem">
+            <Link className="navLinkItem " to="/celtics">
+              <SearchOutlinedIcon/>
+            </Link>
+            <Link className="navLinkItem" to="/celtics">
+              <ShoppingCartOutlinedIcon style={{marginLeft:"20px"}}/>
+            </Link>
+          </li>
+        </ul>
+    </Nav>
+  </Navbar.Collapse>
+         </div>
+      </Navbar>
+  
+</>
+    )
+}
+
+export default NavbarJS
+
